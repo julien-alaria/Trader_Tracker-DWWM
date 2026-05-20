a81030f26150fe40aafd0d81a6053a97982f309a
julien-alaria <julienalaria@gmail.com>
2026-05-20 15:00:29 +0200

    implementation of getUsers function in UserModel.js

--- back/src/models/UserModel.js
+++ back/src/models/UserModel.js
@@ -1,1 +1,11 @@
 import getConnection from "../db/connection.js"
+
+async function getUsers() {
+    const db = getConnection()
+
+    const [rows] = await db.query("SELECT * FROM users")
+
+    return rows
+}
+
+export default { getUsers }
